e2df727fa500802687c507c47ff2902663d49999
Tessera Crockett <tessera.crockett@selu.edu>
2020-11-12 14:02:05 -0600

    function two and updates

--- R/R_pkg/calc_finalheight_diff.R
+++ R/R_pkg/calc_finalheight_diff.R
@@ -1,6 +1,6 @@
 #'Calculate final plant height into new column
 #'
-#'@param complete_exotox_data name of tibble 'complete_ecotox_data' (tibble, no default)
+#'@param ecotox name of tibble 'ecotox' (tibble, no default)
 #'@param HEIGHT_F final height of plant in cm (numeric, no default)
 #'@param HEIGHT_I initial height of plant in cm (numeric, no default)
 #'@return difference (numeric)
@@ -8,10 +8,12 @@
 #'@examples 
 #'calc_finalheight_diff (HEIGHT_F-HEIGHT_I)
 #'
-calc_finalheight_diff <- function(data = complete_ecotox_data,
-                                  HEIGHT_F = complete_ecotox_data$HEIGHT_F,
-                                  HEIGHT_I = complete_ecotox_data$HEIGHT_I)
-{fh_diff <- data %>% mutate(fh_diff = 
-                              (complete_ecotox_data$HEIGHT_F
-                               -complete_ecotox_data$HEIGHT_I))
+calc_finalheight_diff <- function(data = ecotox,
+                                  HEIGHT_F = ecotox$HEIGHT_F,
+                                  HEIGHT_I = ecotox$HEIGHT_I)
+{assert_that(is.numeric(HEIGHT_F))
+  assert_that(is.numeric(HEIGHT_I))
+  fh_diff <- data %>% mutate(fh_diff = 
+                              (ecotox$HEIGHT_F
+                               -ecotox$HEIGHT_I))
 return(fh_diff)}
--- R/R_pkg/calc_root_shoot_ratio.R
+++ R/R_pkg/calc_root_shoot_ratio.R
@@ -1,6 +1,6 @@
 #'Calculate root/shoot ratio into new column
 #'
-#'@param complete_exotox_data name of tibble 'complete_ecotox_data' (tibble, no default)
+#'@param ecotox name of tibble 'ecotox' (tibble, no default)
 #'@param BELOWGROUND weight of BELOWGROUND biomass in grams (numeric, no default)
 #'@param ABOVE_GROUND weight of the ABOVE_GROUND biomass in grams (numeric, no default)
 #'@return ratio (numeric)
@@ -8,11 +8,11 @@
 #'@examples 
 #'calc_root_shoot_ratio (BELOWGROUND/ABOVE_GROUND)
 #'
-calc_rootshoot_ratio <- function(data = complete_ecotox_data,BELOWGROUND = complete_ecotox_data$BELOWGROUND,
-                                    ABOVE_GROUND = complete_ecotox_data$ABOVE_GROUND)
+calc_rootshoot_ratio <- function(data = ecotox,BELOWGROUND = ecotox$BELOWGROUND,
+                                    ABOVE_GROUND = ecotox$ABOVE_GROUND)
 {rs_ratio <- data %>% mutate(rs_ratio = 
-                                                    (complete_ecotox_data$BELOWGROUND
-                                                    /complete_ecotox_data$ABOVE_GROUND))
+                                                    (ecotox$BELOWGROUND
+                                                    /ecotox$ABOVE_GROUND))
 return(rs_ratio)}
 
 
